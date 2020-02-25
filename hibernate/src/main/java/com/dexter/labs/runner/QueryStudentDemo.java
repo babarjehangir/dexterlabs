fbef1f0e1f743b68703802b2ca709567438639fd
Babar Khan <babarjehangir@gmail.com>
2020-02-25 14:27:21 -0500

    In order to see parameter values which hibernate is using in logs

--- hibernate/src/main/java/com/dexter/labs/runner/QueryStudentDemo.java
+++ hibernate/src/main/java/com/dexter/labs/runner/QueryStudentDemo.java
@@ -1,5 +1,7 @@
 package com.dexter.labs.runner;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -20,15 +22,42 @@ public class QueryStudentDemo {
 		try {
 
 			session.beginTransaction();
+			
+			List<Student> theStudents = session.createQuery("from Student").getResultList();
+			
+			displayStudents(theStudents);
+			
+			
+			theStudents = session.createQuery("from Student s where s.lastName='Doe'").getResultList();
 
+			displayStudents(theStudents);
+			
+			theStudents = session.createQuery("from Student s where s.lastName='Doe' OR s.firstName='Shadow'").getResultList();
+
+			System.out.println("\nStudents with last name Doe or first name Shadow");
+			
+			displayStudents(theStudents);
+			
+			theStudents = session.createQuery("from Student s where s.email LIKE '%dummy.com'").getResultList();
+
+			System.out.println("\nStudents with email like dummy.com");
+			
+			displayStudents(theStudents);
+			
 			session.getTransaction().commit();
-
+			
 			System.out.println("Done");
 
 		} finally {
 			factory.close();
 		}
 
+	}
+
+	private static void displayStudents(List<Student> theStudents) {
+		for(Student tStudent : theStudents) {
+			System.out.println(tStudent);
+		}
 	}
 
 }
